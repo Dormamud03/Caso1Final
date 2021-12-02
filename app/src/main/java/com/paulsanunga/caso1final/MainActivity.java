de4b9c8a5d8fd8622b7cfe2d986da54d26d1cab3
Dormamud03 <paulsanunga@gmail.com>
2021-12-01 20:37:33 -0500

    Initial commit

---
+++ app/src/main/java/com/paulsanunga/caso1final/MainActivity.java
@@ -0,0 +1,14 @@
+package com.paulsanunga.caso1final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
